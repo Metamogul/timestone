ffcabe5f8da3c9711365eccb5e23906495660a37
Jan Zombik <jan.zombik@gridfuse.com>
2024-09-11 19:37:49 +0200

    add more go docs

--- simulation/scheduler.go
+++ simulation/scheduler.go
@@ -22,6 +22,8 @@ type Scheduler struct {
 	finishedEventsWaitGroups *waitGroups
 }
 
+// NewScheduler will return a new Scheduler instance, with its
+// clock initialized to return now.
 func NewScheduler(now time.Time) *Scheduler {
 	eventConfigs := newEventConfigurations()
 
@@ -33,14 +35,26 @@ func NewScheduler(now time.Time) *Scheduler {
 	}
 }
 
+// SetDefaultMode will set a default ExecMode that is applied
+// to all events at execution time that don't have their ExecMode
+// individually configured via ConfigureEvent. If no default ExecMode
+// is provided, the Scheduler will panic once it reaches an Event that
+// doesn't have an EventConfiguration with a ScheduleMode set to something
+// else than ExecModeUndefined.
 func (s *Scheduler) SetDefaultMode(mode ExecMode) {
 	s.eventConfigs.defaultExecMode = mode
 }
 
+// ConfigureEvent provides an EventConfiguration for either a single event,
+// identified by the name of its embedded action and the time of its occurence,
+// or for every event matching the actionName if no time is provided.
 func (s *Scheduler) ConfigureEvent(actionName string, time *time.Time, config EventConfiguration) {
 	s.eventConfigs.set(actionName, time, config)
 }
 
+// ForwardOne executes just the next event that is scheduled on the
+// event queue of the Scheduler, and sets the timestone.Clock of the Scheduler
+// to the time of the event.
 func (s *Scheduler) ForwardOne() {
 	s.eventGeneratorsMu.RLock()
 
@@ -55,14 +69,37 @@ func (s *Scheduler) ForwardOne() {
 	s.scheduleEvent(nextEvent)
 }
 
+// WaitFor is to be used after ForwardOne and blocks until all scheduled
+// events embedding actions with the specified actionNames have finished.
 func (s *Scheduler) WaitFor(actionNames ...string) {
 	s.finishedEventsWaitGroups.waitFor(actionNames...)
 }
 
+// Wait is to be used after ForwardOne and blocks until all scheduled
+// events have finished.
 func (s *Scheduler) Wait() {
 	s.finishedEventsWaitGroups.wait()
 }
 
+// Forward will forward the Scheduler.Clock while running all events to
+// occur until Scheduler.Clock.Now() + interval. Each action will receive in
+// its context.Context a timestone.Clock set to return the respective execution
+// time for Now().
+//
+// Depending on their individual configuration, Event s will either be run
+// sequentially, waiting for all preciously started Event s to finish, or
+// asynchronously.
+//
+// Event s will be materialized and executed from the schedulers event queue in
+// temporal order. In case of simultaneousness, the exection order can be changed
+// with the EventConfiguration.Priority passed through ConfigureEvent.
+//
+// Event s configured via their EventConfiguration.WaitForActions will only start
+// execution once the specified events have finished.
+//
+// Event s configured via EventConfiguration.WantsNewGenerators will block the run
+// loop until the specified EventGenerator instances have been passed to the
+// Scheduler, either via one of the Perform... methods or via AddEventGenerators.
 func (s *Scheduler) Forward(interval time.Duration) {
 	targetTime := s.clock.Now().Add(interval)
 
@@ -130,18 +167,33 @@ func (s *Scheduler) scheduleEvent(event *Event) {
 	s.eventQueue.newGeneratorsWaitGroups.waitFor(wantedActionNames...)
 }
 
+// PerformNow schedules action to be executed immediately, that is
+// at the current time of the Scheduler's clock. It adds a new Event
+// generator which materializes a corresponding event to the Scheduler's
+// event queue.
 func (s *Scheduler) PerformNow(ctx context.Context, action timestone.Action) {
 	s.AddEventGenerators(newSingleEventGenerator(ctx, action, s.now))
 }
 
+// PerformAfter schedules an action to be run once after a delay
+// of duration. It adds a new Event  generator which materializes a
+// corresponding event to the Scheduler's event queue.
 func (s *Scheduler) PerformAfter(ctx context.Context, action timestone.Action, interval time.Duration) {
 	s.AddEventGenerators(newSingleEventGenerator(ctx, action, s.now.Add(interval)))
 }
 
+// PerformRepeatedly schedules an action to be run every interval
+// after an initial delay of interval. If until is provided, the last
+// event will be run before or at until. It adds a new Event
+// generator which materializes corresponding events to the Scheduler's
+// event queue.
 func (s *Scheduler) PerformRepeatedly(ctx context.Context, action timestone.Action, until *time.Time, interval time.Duration) {
 	s.AddEventGenerators(newPeriodicEventGenerator(ctx, action, s.Now(), until, interval))
 }
 
+// AddEventGenerators is used by the Perform... methods of the Scheduler.
+// It can be used to pass a custom event generator if Timestone is used
+// to run event-based simulations.
 func (s *Scheduler) AddEventGenerators(generators ...EventGenerator) {
 	s.eventGeneratorsMu.Lock()
 	defer s.eventGeneratorsMu.Unlock()
